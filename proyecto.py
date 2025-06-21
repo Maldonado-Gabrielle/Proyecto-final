37242a8683fdf1bfad3b5c1b3efe26d02cb8e477
Maldonado Gabrielle <monse.meneses155@gmail.com>
2025-06-20 20:23:41 -0600

    Create proyecto.py

---
+++ proyecto.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+import tkinter.messagebox as messagebox
+from datetime import datetime
+
+lista_trabajadores_registrados = []
+contadorun = 0
+contadordo = 0
+contadortr = 0
+contadorcu = 0
+boton_vacu = None
+boton_vacd = None
+boton_vact = None
+boton_vacc = None
+historial_actividades = []
